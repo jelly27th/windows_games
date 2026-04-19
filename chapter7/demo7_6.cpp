2b040fde78653e276bc24fbfa25d9c92147a7db2
Jelly27th <872908391@qq.com>
2026-04-19 23:48:00 +0800

    add demo7_6 code

---
+++ chapter7/demo7_6.cpp
@@ -0,0 +1,309 @@
+/* 
+  8-bit page flipping demo
+*/
+
+#define WIN32_LEAN_AND_MEAN  // just say no to MFC
+
+#define INITGUID // make sure the directx GUIDs are included
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#include <windows.h>
+#include <windowsx.h>
+#include <mmsystem.h>
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <math.h>
+#include <io.h>
+#include <fcntl.h>
+
+#include <ddraw.h> // include directdraw
+
+#define WINDOW_CLASS_NAME L"WINCLASS1"
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define SCREEN_BPP 16 // bits per pixel
+
+/* basic unsigned types */
+typedef unsigned short USHORT;
+typedef unsigned short WORD;
+typedef unsigned char UCHAR;
+typedef unsigned char BYTE;
+
+/* macro */
+#define KEYDOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
+#define KEYUP(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
+
+/* this build a 16 bit color value in 5.5.5 format (1-bit alpha mode) */
+#define _RGB16BIT555(r,g,b) ((b & 0x1f) + ((g & 0x1f) << 5) + ((r & 0x1f) << 10))
+
+/* this build a 16 bit color value in 5.6.5 format (green dominate mode) */
+#define _RGB16BIT565(r, g, b) ((b & 0x1f) + ((g & 0x3f) << 5) + ((r & 0x1f) << 11))
+
+// this builds a 32 bit color value in A.8.8.8 format (8-bit alpha mode)
+#define _RGB32BIT(a,r,g,b) ((b) + (g << 8) + (r << 16) + (a << 24))
+
+/* initialize a direct draw structure */
+#define DDRAW_INIT_STRUCT(ddstruct) { memset(&ddstruct, 0, sizeof(ddstruct)); ddstruct.dwSize = sizeof(ddstruct); }
+
+/* global variables */
+HWND main_window_handle = NULL; // global track main window
+int window_closed = 0; // track if window is closed
+HINSTANCE hinstance_app = NULL; // global track hinstance
+
+/* directdraw stuff*/
+LPDIRECTDRAW7 lpdd = NULL;                   // the directdraw object
+LPDIRECTDRAWSURFACE7 lpddsprimary = NULL;    // the directdraw primary surface
+LPDIRECTDRAWSURFACE7 lpddsback = NULL;       // the directdraw back surface
+LPDIRECTDRAWPALETTE   lpddpal      = NULL;   // a pointer to the created dd palette
+LPDIRECTDRAWCLIPPER lpddclipper = NULL;      // the directdraw clipper
+PALETTEENTRY          palette[256];          // color palette
+PALETTEENTRY          save_palette[256];     // used to save palettes
+DDSURFACEDESC2 ddsd; // a direct draw surface description structure
+DDBLTFX ddbltfx; // used to fill
+DDSCAPS2 ddscaps; // a direct draw surface capabilities structure
+HRESULT ddrval; // result back from directdraw calls
+DWORD start_clock_count = 0; // used for timing
+
+char buffer[80]; // general printing buffer
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+  
+  PAINTSTRUCT ps;
+  HDC hdc;
+  char buffer[80]; // used to print strings
+
+  switch (msg) {
+    case WM_CREATE: {
+      // do initialization stuff here
+
+      return 0;
+    } break;
+    
+    case WM_PAINT: {
+      // simple validate the window
+      hdc = BeginPaint(hwnd, &ps);
+      // you would do your painting here
+
+      EndPaint(hwnd, &ps);
+      return 0;
+    } break;
+
+    case WM_DESTROY: {
+      // kill the application, this sends a WM_QUIT message
+      PostQuitMessage(0);
+      return 0;
+    } break;
+
+    default:
+      break;
+  }
+
+  // process any message that we didn't take care of
+  return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+/* main game loop */
+int Game_Main(void* parms = NULL, int num_parms = 0) {
+
+   DDBLTFX ddbltfx; // teh bitter fx structer
+   RECT dest_rect; // used to hold the destination RECT
+
+   // make sure this isn't executed again
+   if (window_closed) return 0;
+
+    // for now test if the user is hitting ESC and send WM_CLOSE
+    if (KEYDOWN(VK_ESCAPE)) {
+      PostMessage(main_window_handle, WM_CLOSE, 0, 0);
+      window_closed = 1;
+    }
+
+    /* copy the double buffer into the primary buffer */
+    DDRAW_INIT_STRUCT(ddbltfx);
+
+    // now set the color word info to the color we desire
+    // in this case, we are assuming an 8-bit mode, hence,
+    // well use a color index from 0-255, but if this was a 
+    // 16/24/32 bit example then we would fill the WORD with
+    // the RGB encoding for the pixel - remember!
+    ddbltfx.dwFillColor = _RGB16BIT565(rand()%256,rand()%256,rand()%256);
+
+    // get a random rectangle
+    int x1 = rand()%SCREEN_WIDTH;
+    int y1 = rand()%SCREEN_HEIGHT;
+    int x2 = rand()%SCREEN_WIDTH;
+    int y2 = rand()%SCREEN_HEIGHT;
+
+    // now set up the RECT structure to fill the region from
+    // (x1,y1) to (x2,y2) on the destination surface
+    dest_rect.left = x1;
+    dest_rect.top = y1;
+    dest_rect.right = x2;
+    dest_rect.bottom = y2;
+
+    // make the blitter call
+    if (FAILED(lpddsprimary->Blt(&dest_rect, // pointer to the dest RECT
+                                  NULL, // pointer to source surface
+                                  NULL, // pointer to source RECT
+                                  DDBLT_COLORFILL|DDBLT_WAIT,
+                                  // do a color fill and wait if you have to
+                                  &ddbltfx // pointer to DDBLTFX holding info
+                                  ))) {
+        return 0;
+    }
+
+    // return success or failure or your own return code here
+    return 1;
+}
+
+/*  
+    this is call once after the initial window is created  and
+    before the main game loop is entered
+*/
+int Game_Init(void* parms = NULL, int num_parms = 0) {
+
+    // create IDirectDraw instance 7.0 object and test for error
+    if (FAILED(DirectDrawCreateEx(NULL, (void**)&lpdd, IID_IDirectDraw7, NULL))) {
+        return 0;
+    }
+
+    // set cooperation to full screen 
+    if (FAILED(lpdd->SetCooperativeLevel(main_window_handle, 
+                                         DDSCL_FULLSCREEN | DDSCL_EXCLUSIVE | 
+	                                       DDSCL_ALLOWMODEX | DDSCL_ALLOWREBOOT))) {
+        return 0;
+    }
+
+    if (FAILED(lpdd->SetDisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, 0, 0))) {
+      return 0;
+    }
+
+    // clear ddsd and set size
+    DDRAW_INIT_STRUCT(ddsd);
+
+    // enable valid fields
+    ddsd.dwFlags =  DDSD_CAPS;
+
+    // request a complex, flipable
+    ddsd.ddsCaps.dwCaps =  DDSCAPS_PRIMARYSURFACE;
+
+    // create the primary surface
+    if (FAILED(lpdd->CreateSurface(&ddsd, &lpddsprimary, NULL))) {
+      return 0;
+    }
+    
+    return 1;
+}
+
+/* 
+    this is called after the game is exited and the main event loop 
+    while is exited
+*/
+int Game_Shutdown(void* parms = NULL, int num_parms = 0) {
+    
+    // now the primary surface
+    if (lpddsprimary) {
+      lpddsprimary->Release();
+      lpddsprimary = NULL;
+    }    
+
+    // now blow away the IDirectDraw4 interface
+    if (lpdd) {
+        lpdd->Release();
+        lpdd = NULL;
+    }
+
+    return 1;
+}
+
+/* main entry point */
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance,
+                   LPSTR lpcmdline, int ncmdshow) {
+  WNDCLASSEX winclass;
+  HWND hwnd;
+  MSG msg;
+  HDC hdc;
+
+  // first fill in the window class structure
+  winclass.cbSize = sizeof(WNDCLASSEX);
+  winclass.style = CS_DBLCLKS | CS_OWNDC | 
+                   CS_HREDRAW | CS_VREDRAW;
+  winclass.lpfnWndProc = WindowProc;
+  winclass.cbClsExtra = 0;
+  winclass.cbWndExtra = 0;
+  winclass.hInstance = hinstance;
+  winclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  winclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+  winclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+  winclass.lpszMenuName = NULL;
+  winclass.lpszClassName = WINDOW_CLASS_NAME;
+  winclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+  // save instance in global
+  hinstance_app = hinstance;
+  
+  // register the window class
+  if (!RegisterClassEx(&winclass)) {
+    return 0;
+  }
+
+  // create the window
+  if (!(hwnd = CreateWindowEx((DWORD)NULL,           // extended style
+                              WINDOW_CLASS_NAME,    // class
+                              L"Demo7_6",  // title
+                              WS_POPUP | WS_VISIBLE,
+                              0,0,          // initial x, y
+                              SCREEN_WIDTH, SCREEN_HEIGHT, // initial width, height   
+                              NULL,       // handle to parent
+                              NULL,       // handle to menu
+                              hinstance,  // histance of this application
+                              NULL)))     // extra creation parms
+  {
+    return 0;
+  }
+
+  // save the main window handle
+  main_window_handle = hwnd;
+
+  // initialize the game
+  Game_Init();
+
+  // enter main event loop, but this time we use PeekMessage()
+  // instead of GetMessage() to retrieve messages
+  while (TRUE) {
+
+    // test if there is a message in queue, if so get it
+    if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+      // test if this is a quit
+      if (WM_QUIT == msg.message) break;
+      // translate any accelerator keys
+      TranslateMessage(&msg);
+
+      // send the message to the window proc
+      DispatchMessage(&msg);
+
+    }
+
+    /* main game processing goes here */
+    Game_Main();
+
+  }
+
+   // close dowm the game here
+   Game_Shutdown();
+
+  return msg.wParam;
+}
